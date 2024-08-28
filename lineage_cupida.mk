6437710e712f5ab4d619ecf07845d7626200aba2
subhagamer <lordkazuya1@gmail.com>
2024-08-28 10:04:27 +0000

    cupida: Init TenX Official

--- lineage_cupida.mk
+++ lineage_cupida.mk
@@ -24,6 +24,12 @@ $(call inherit-product, device/oplus/cupida/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# TenX Flags
+TENX_BUILD_TYPE=Official
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_cupida
 PRODUCT_DEVICE := cupida
