d24a3ce9d6322b9599111f44fd66a8df487780ac
subhagamer <lordkazuya1@gmail.com>
2024-09-02 10:20:57 +0530

    cupida:update vendorsetup.sh with TenX projects & shallow clone projects

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,28 +3,28 @@
 
 echo start cloning repos and bringups
 # common dt
-git clone https://github.com/oplus-ossi-development/android_device_oplus_mt6893-common -b lineage-21 device/oplus/mt6893-common && rm device/oplus/mt6893-common/vendorsetup.sh
+git clone https://github.com/Ten-X-Devices/device_oplus_mt6893-common device/oplus/mt6893-common --depth=1 && rm device/oplus/mt6893-common/vendorsetup.sh
 # proprietary_vendor
-git clone https://github.com/oplus-ossi-development/proprietary_vendor_oplus_mt6893-common vendor/oplus/mt6893-common
+git clone https://github.com/Ten-X-Devices/vendor_oplus_mt6893-common --depth=1 vendor/oplus/mt6893-common
 # hardware mediatek
 rm -rf hardware/mediatek && git clone https://github.com/oplus-ossi-development/android_hardware_mediatek hardware/mediatek
 # oneplus parts
-git clone https://github.com/oplus-ossi-development/android_packages_apps_OneplusParts packages/apps/OneplusParts
+git clone https://github.com/oplus-ossi-development/android_packages_apps_OneplusParts packages/apps/OneplusParts --depth=1
 # kernel dt
-git clone --depth=1 --recurse-submodules https://github.com/oplus-ossi-development/android_kernel_oplus_mt6893 kernel/oplus/mt6893
+git clone --depth=1 --recurse-submodules https://github.com/Ten-X-Devices/kernel_oplus_mt6893 kernel/oplus/mt6893
 # hardware mediatek wlan
-git clone https://github.com/oplus-ossi-development/android_hardware_mediatek_wlan hardware/mediatek/wlan
+git clone https://github.com/oplus-ossi-development/android_hardware_mediatek_wlan hardware/mediatek/wlan --depth=1
 # vendor oplus cupida
-git clone https://github.com/oplus-ossi-development/proprietary_vendor_oplus_cupida vendor/oplus/cupida
-# sepolicy vndr 
-git clone https://github.com/oplus-ossi-development/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
-# oplus hardware 
-rm -rf hardware/oplus && git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
+git clone https://github.com/Ten-X-Devices/vendor_oplus_cupida vendor/oplus/cupida --depth=1
+# sepolicy vndr
+git clone https://github.com/oplus-ossi-development/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr --depth=1
+# oplus hardware
+rm -rf hardware/oplus && git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus --depth=1
 
 # Oplus camera
 CAM=device/oplus/camera/camera.mk
 if ! [ -a $CAM ]; then
-    git clone https://gitlab.com/nattolecats/android_device_oplus_camera device/oplus/camera
+    git clone https://gitlab.com/nattolecats/android_device_oplus_camera device/oplus/camera --depth=1
 fi
 
 cd device/oplus/camera/ && git lfs pull && cd ../../../
@@ -32,7 +32,7 @@ cd device/oplus/camera/ && git lfs pull && cd ../../../
 # Clang
 CLANG17=prebuilts/clang/host/linux-x86/clang-r487747/bin/clang
 if ! [ -a $CLANG17 ]; then
-    git clone https://gitlab.com/nattolecats/android_prebuilts_clang_host_linux-x86_clang-r487747 prebuilts/clang/host/linux-x86/clang-r487747 --depth 1
+    git clone https://gitlab.com/nattolecats/android_prebuilts_clang_host_linux-x86_clang-r487747 prebuilts/clang/host/linux-x86/clang-r487747 --depth=1
 fi
 
 # Bypass API modified validations
@@ -46,7 +46,7 @@ mv device/oplus/RMX3031 device/oplus/cupida &> /dev/null
 bash device/oplus/mt6893-common/patches/apply.sh
 
 #end cloning
-echo bringup successful 
+echo bringup successful
 
 #delete setup
 rm device/oplus/cupida/vendorsetup.sh
